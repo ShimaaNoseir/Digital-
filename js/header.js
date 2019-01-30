d6231e858900bd26fc7077026a503bb2628dba4d
= <=>
2019-01-30 21:24:58 +0200

    first commit

---
+++ js/header.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myNavbar").style.top = "0";
+  } else {
+    document.getElementById("myNavbar").style.top = "60px";
+  }
+}
+
+
+/*navbar*/
+
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
